1e0557b27dc645df73dd55b5bb0f019324298474
Jernej Azarija <jernej.azarija@gmail.com>
2018-12-03 18:45:51 +0100

    Added function to download bitcoin core and glacier

--- icecube.sh
+++ icecube.sh
@@ -1,57 +1,122 @@
-#### INIT 
-sudo apt-get install debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
-mkdir $HOME/LIVE_BOOT
-#### INIT
+# DOCUMENT ME
+BITCOINCORE_FINGERPRINT="01EA5486DE18A882D4C2684590C8019E36C2E964"
+GLACIER_FINGERPRINT="E1AAEBB7AC90C1FE80F010349D1B7F534B43EAB0"
 
-#### CREATE THE BASE SYSTEM 
-sudo debootstrap --arch=amd64 --variant=minbase stretch $HOME/LIVE_BOOT/chroot http://ftp.us.debian.org/debian/
 
-# this section needs to be executed within chroot
-# this is the place to tweak to add/remove packages from the base system
-sudo chroot "$HOME/LIVE_BOOT/chroot" bash << 'EOF'
-printf 'icecube\n' > /etc/hostname
+function init_environment() {
 
-export DEBIAN_FRONTEND=noninteractive
-apt-get update &&
-apt-get install --no-install-recommends -y \
-    linux-image-amd64 live-boot systemd-sysv blackbox xserver-xorg-core \
-    xserver-xorg xinit xterm # vim qrencode zbar-tools &&
+    sudo apt-get install debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
+    mkdir $HOME/LIVE_BOOT
 
-apt-get clean
-# FIXME Make Xorg start automatically and remove this bit 
-echo "root:icecube" | chpasswd
+}
+
+function create_base_system() {
+    sudo debootstrap --arch=amd64 --variant=minbase stretch $HOME/LIVE_BOOT/chroot http://ftp.us.debian.org/debian/
+
+    # this section needs to be executed within chroot
+    # this is the place to tweak to add/remove packages from the base system
+    sudo chroot "$HOME/LIVE_BOOT/chroot" bash << 'EOF'
+    printf 'icecube\n' > /etc/hostname
+
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update &&
+    apt-get install --no-install-recommends -y \
+        linux-image-amd64 live-boot systemd-sysv blackbox xserver-xorg-core \
+        xserver-xorg xinit xterm # vim qrencode zbar-tools &&
+
+    apt-get clean
+    # FIXME Make Xorg start automatically and remove this bit 
+    echo "root:icecube" | chpasswd
+# FIXME try to indent that
 EOF
 
 
 
-mkdir -p $HOME/LIVE_BOOT/{scratch,image/live}
 
-sudo mksquashfs $HOME/LIVE_BOOT/chroot $HOME/LIVE_BOOT/image/live/filesystem.squashfs -e boot
+}
+
+
+
+# Routine that safely downloads bitcoin core. 
+# FIXME. It is crucial to audit this thorougly.
+function install_bitcoincore() {
+   
+    wget https://bitcoincore.org/bin/bitcoin-core-0.17.0.1/SHA256SUMS.asc
+    wget https://bitcoincore.org/bin/bitcoin-core-0.17.0.1/bitcoin-0.17.0.1-x86_64-linux-gnu.tar.gz
+
+    if ! sha256sum --ignore-missing --check SHA256SUMS.asc ; then
+        print 'Something is awfully wrong. Aborting.\n'   
+        exit
+    fi   
+
+    gpg --recv-keys $BITCOINCORE_FINGERPRINT 
+    
+    out=$(gpg --status-fd 1 --verify SHA256SUMS.asc 2>/dev/null)
+    
+    if ! (echo "$out" | grep "GOODSIG" && echo "$out" | grep "VALIDSIG $BITCOINCORE_FINGERPRINT") ; then
+        echo "Checking integrity of Bitcoin core failed. Abort protocol"
+        exit
+    fi        
+    tar -xf bitcoin-0.17.0.1-x86_64-linux-gnu.tar.gz -C $HOME/LIVE_BOOT/chroot
+}
+
+function install_glacier() {
+   
+    wget https://github.com/GlacierProtocol/GlacierProtocol/archive/v0.93-beta.tar.gz
+    wget --output-document=glacier.asc  https://keybase.io/glacierprotocol/pgp_keys.asc?fingerprint=e1aaebb7ac90c1fe80f010349d1b7f534b43eab0 
+    gpg --import glacier.asc
+    mkdir glacier && tar -xf "v0.93-beta.tar.gz" -C glacier --strip-components 1 && cd glacier
+    out=$(gpg --status-fd 1 --verify SHA256SUMS.sig SHA256SUMS 2>/dev/null)
+    
+    if ! (echo "$out" | grep "GOODSIG" && echo "$out" | grep "VALIDSIG $GLACIER_FINGERPRINT") ; then
+        echo "Checking integrity of Glacier failed. Abort protocol"
+        exit
+    fi
+
+    cd .. 
+    mv glacier $HOME/LIVE_BOOT/chroot/root
+}
+
 
 ### SETUP THE BOOTLOADER
-cp $HOME/LIVE_BOOT/chroot/boot/vmlinuz-* $HOME/LIVE_BOOT/image/vmlinuz  
-cp $HOME/LIVE_BOOT/chroot/boot/initrd.img-* $HOME/LIVE_BOOT/image/initrd
-
-cat <<'EOF' >$HOME/LIVE_BOOT/scratch/grub.cfg
-
-search --set=root --file /DEBIAN_CUSTOM
-
-insmod all_video
-
-set default="0"
-set timeout=0
-
-menuentry "icecube" {
-    linux /vmlinuz boot=live quiet nomodeset
-    initrd /initrd
-}
-EOF
-touch $HOME/LIVE_BOOT/image/DEBIAN_CUSTOM
 
 #### MAKE BOOTABLE USB
 
-# FIXME Make this a command line argument
+function setup_bootable_USB() {
+    mkdir -p $HOME/LIVE_BOOT/{scratch,image/live}
+
+    sudo mksquashfs $HOME/LIVE_BOOT/chroot $HOME/LIVE_BOOT/image/live/filesystem.squashfs -e boot
+
+    cp $HOME/LIVE_BOOT/chroot/boot/vmlinuz-* $HOME/LIVE_BOOT/image/vmlinuz  
+    cp $HOME/LIVE_BOOT/chroot/boot/initrd.img-* $HOME/LIVE_BOOT/image/initrd
+
+    cat <<'EOF' >$HOME/LIVE_BOOT/scratch/grub.cfg
+
+    search --set=root --file /DEBIAN_CUSTOM
+
+    insmod all_video
+
+    set default="0"
+    set timeout=0
+
+    menuentry "icecube" {
+        linux /vmlinuz boot=live quiet nomodeset
+        initrd /initrd
+    }
+EOF
+
+
+    touch $HOME/LIVE_BOOT/image/DEBIAN_CUSTOM
+
+    # FIXME Make this a command line argument
 export disk=/dev/sda
+
+#FIXME
+
+# Maybe add sudo dd if=/dev/zero of=$disk bs=1k count=2048
+
+#or, rather sudo mkfs.vfat /dev/sdZ
+
 
 sudo mkdir -p /mnt/{usb,efi}
 sudo parted --script $disk \
@@ -112,3 +177,12 @@ sudo cp \
 
 sudo umount /mnt/{usb,efi}
 
+}
+
+
+#init_environment
+#create_base_system
+
+install_glacier
+install_bitcoincore
+setup_bootable_USB
